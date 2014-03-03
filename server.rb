86d76e0afcde0740e836ee39242153d366dd1aa0
Hannah Zabarsky <h.zabarsky@gmail.com>
2014-03-03 08:37:44 -0500

    All ruby code working (including actual sort method)

--- server.rb
+++ server.rb
@@ -1,8 +1,8 @@
-#require 'sinatra'
+# require 'sinatra'
 require 'pry'
 
-#get '/leaderboard' do
-#erb :index
+# get '/leaderboard' do
+# erb :index
 
 leaderboard = {}
 matches = [
@@ -32,6 +32,8 @@ matches = [
   }
 ]
 
+
+#probably should refactor to use a method
 matches.each do |match|
  home = {match.fetch(:home_team) => {:win => 0 , :loss => 0}}
     leaderboard.merge!(home)
@@ -40,7 +42,7 @@ matches.each do |match|
 end
 
 
-
+#could probably use a method within this but no time to refactor it currently
 matches.each do |playoff|
  if playoff.fetch(:home_score).to_i > playoff.fetch(:away_score).to_i
    winner = playoff.fetch(:home_team)
@@ -55,7 +57,9 @@ matches.each do |playoff|
   end
 end
 
+#|k,v| [-v,k] is what i need to implement
 
-@leaderboard = leaderboard.sort_by { |k,v| v[:win] }.reverse
-
+leaderboard2 = leaderboard.sort_by { |key, value| [value[:win], -value[:loss]] }.reverse
+leaderboard3 = leaderboard2.sort_by
+binding.pry
 
