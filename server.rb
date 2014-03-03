c3ffd30c1dee695749500ca8a402dcf5ae863dd8
Hannah Zabarsky <h.zabarsky@gmail.com>
2014-03-03 03:49:39 -0500

    everything fixed and working except sinatra

--- server.rb
+++ server.rb
@@ -1,5 +1,9 @@
-# require 'sinatra'
+#require 'sinatra'
 require 'pry'
+
+#get '/leaderboard' do
+#erb :index
+
 leaderboard = {}
 matches = [
   {
@@ -29,10 +33,10 @@ matches = [
 ]
 
 matches.each do |match|
-  home = {match.fetch(:home_team) => {:win => 0 , :loss => 0}}
+ home = {match.fetch(:home_team) => {:win => 0 , :loss => 0}}
     leaderboard.merge!(home)
-  away = {match.fetch(:away_team) => {:win => 0 , :loss => 0}}
-    leaderboard.merge!(home)
+ away = {match.fetch(:away_team) => {:win => 0 , :loss => 0}}
+    leaderboard.merge!(away)
 end
 
 
@@ -52,58 +56,6 @@ matches.each do |playoff|
 end
 
 
-
-#so after all of this we should have leaderboard = [{teams win loss}]
-#leaderboard2 = leaderboard.sort_by {http://www.gethourglass.com/blog/ruby-how-to-sort-a-hash.html}
+@leaderboard = leaderboard.sort_by { |k,v| v[:win] }.reverse
 
 
-
-
-
-#leaderboard(get_value(:home_team))(:win)) +=1
-   #puts winner
-#      #puts match[:win] = (leaderboard.fetch(:win)).to_i += 1.to_i
-#     #retrieve value of :wins from leaderboard.to_i +=1
-#     #.merge! new value to :wins
-#   #find :away_team in leaderboard
-#    #retrieve value of losses from leaderboard.to_i +=1
-#    #.merge! new value to :loss
-
-
-
-
-
-
-
-
-# def find_team (matches, find_team)
-
-# end
-
-
-# cc = Hash.new(0)
-# #goes through every existing hash for information
-#  matches.each do |match|
-#   leaderboard.select {|team| team[match.fetch(find_team)]}
-#   #if the home score is bigger than the away score
-#   if
-#     #find the winning team name in the array with all teams, if it already exists
-#     if find_team(:home_team).merge!(:win)
-
-
-#     #leaderboard << {:team => match.fetch(:home_team)}
-#     #add one to the tally "win"
-#     #find the losing team name
-#     #add one to the tally "loss"
-
-
-#     end
-#   else
-#     leaderboard << {:item1 => 1}
-#     leaderboard << {:item1 => 1}
-#   end
-# end
-
-# puts cc
-# puts matches
-# puts leaderboard
